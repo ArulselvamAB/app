ca61bb8589c7d08a38aa9962f935007137bc09ff
ArulselvamAB <arulab2002@gmail.com>
2023-08-25 18:29:56 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponentComponent } from './registrationcomponent/registrationcomponent.component';
+import { LoginComponentComponent } from './logincomponent/logincomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/', pathMatch:'full'},
+  {path: 'login', component: LoginComponentComponent},
+  {path: 'register',component:RegistrationComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/registrationcomponent/registrationcomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-component',
+  templateUrl: './registrationcomponent.component.html',
+  styleUrls: ['./registrationcomponent.component.css']
+})
+export class RegistrationComponentComponent {
+
+}
